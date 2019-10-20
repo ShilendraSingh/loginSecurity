0865cd6b3f8b7245d77610920fc1c0ec26dd9ae9
Shilendra <shuilu1993sr@gmail.com>
2019-10-20 20:01:45 +0530

    Login added

--- src/main/java/com/customer/controller/LoginController.java
+++ src/main/java/com/customer/controller/LoginController.java
@@ -1,8 +1,19 @@
 package com.customer.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.customer.dto.LoginDto;
 
 @RestController
 public class LoginController {
+
+	@PostMapping("/users/login")
+	public ResponseEntity<String> login(@RequestBody LoginDto loginDto){
+		return new ResponseEntity<>("Processing", HttpStatus.OK);
+	}
 
 }
